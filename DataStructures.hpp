8217713fc01ef756f1ed0c0a94493cb0754d59be
Karan-rajendra <145815049+Karan-rajendra@users.noreply.github.com>
2024-02-25 19:48:45 -0500

    Update DataStructures.hpp

--- DataStructures.hpp
+++ DataStructures.hpp
@@ -6,42 +6,42 @@
 
 struct Net; //forward declaration
 
-class Gate { //for now will treat partition as a boolean
+class Gate {
 private:
-	std::string name;
-	std::vector<Net*> nets;
-	bool Part;
-	int GainTot;
-	int area;
+    std::string name;
+    std::vector<Net*> nets;
+    bool Part; // For now, will treat partition as a boolean
+    int GainTot;
+    int area;
 
 public:
-	Gate();
-	Gate(std::string name, std::vector<Net*> nets, bool part, int gain, int area);
-	~Gate();
-	std::vector<Net*> getNets();
-	void addNet(Net* net);
-	void removeNet(Net* net);
-	void setPart();
-	void unsetPart();
-	bool getPart();
-	int getGT();
-	void incGT();
-	void decGT();
-	friend std::ostream& operator<<(std::ostream& os, Gate const& g);
-	std::string getName();
-	int getArea();
-	void setArea(int a);
+    Gate();
+    Gate(std::string name, std::vector<Net*> nets, bool part, int gain, int area);
+    ~Gate();
+    std::vector<Net*> getNets();
+    void addNet(Net* net);
+    void removeNet(Net* net);
+    void setPart();
+    void unsetPart();
+    bool getPart();
+    int getGT();
+    void incGT();
+    void decGT();
+    friend std::ostream& operator<<(std::ostream& os, Gate const& g);
+    std::string getName();
+    int getArea();
+    void setArea(int a);
 
+    // New methods for gain calculation
+    void calculateInitialGain(); // Add this method prototype
+    static void updateGainsAfterMove(Gate* movedGate); // Static method prototype
 };
 
 struct Net {
-	std::string name;
-	std::vector<Gate*> gates;
-	int p1cnt;
-	int p2cnt;
+    std::string name;
+    std::vector<Gate*> gates;
+    int p1cnt;
+    int p2cnt;
 };
 
-#endif // HEADER_HPP
-};
-
-#endif // HEADER_HPP
+#endif // DATASTRUCTURES_HPP

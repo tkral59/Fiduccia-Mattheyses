7821b56b00a5f3af54e5c31d35da08b6484ee63e
Thomas Kral <99615292+tkral59@users.noreply.github.com>
2024-02-25 23:35:54 -0500

    Update DataStructures.hpp

--- DataStructures.hpp
+++ DataStructures.hpp
@@ -6,42 +6,55 @@
 
 struct Net; //forward declaration
 
-class Gate { //for now will treat partition as a boolean
+class Gate {
 private:
-	std::string name;
-	std::vector<Net*> nets;
-	bool Part;
-	int GainTot;
-	int area;
+    std::string name;
+    std::vector<Net*> nets;
+    bool Part; // For now, will treat partition as a boolean
+    int GainTot;
+    int area;
+    int lock = 0;
 
 public:
-	Gate();
-	Gate(std::string name, std::vector<Net*> nets, bool part, int gain, int area);
-	~Gate();
-	std::vector<Net*> getNets();
-	void addNet(Net* net);
-	void removeNet(Net* net);
-	void setPart();
-	void unsetPart();
-	bool getPart();
-	int getGT();
-	void incGT();
-	void decGT();
-	friend std::ostream& operator<<(std::ostream& os, Gate const& g);
-	std::string getName();
-	int getArea();
-	void setArea(int a);
+    Gate();
+    Gate(std::string name, std::vector<Net*> nets, bool part, int gain, int area, int lock);
+    ~Gate();
+    std::vector<Net*> getNets();
+    void addNet(Net* net);
+    void removeNet(Net* net);
+    void setPart();
+    void unsetPart();
+    bool getPart();
+    int getGT();
+    void incGT();
+    void decGT();
+    friend std::ostream& operator<<(std::ostream& os, Gate const& g);
+    std::string getName();
+    int getArea();
+    void setArea(int a);
+    void Lock();
+    void unlock();
 
+    // New methods for gain calculation
+    void calculateInitialGain(); // Add this method prototype
+    static void updateGainsAfterMove(Gate* movedGate); // Static method prototype
 };
 
 struct Net {
-	std::string name;
-	std::vector<Gate*> gates;
-	int p1cnt;
-	int p2cnt;
+    std::string name;
+    std::vector<Gate*> gates;
+    int p1cnt;
+    int p2cnt;
+    bool cut;
 };
 
-#endif // HEADER_HPP
+struct state {
+    std::vector<Gate*> P1; //partition 1 list
+    std::vector<Gate*> P2;
+    int cutsize;
+    int A1; //area 1
+    int A2; //area 2
 };
 
-#endif // HEADER_HPP
+float getCost(std::vector<Net> nets, std::map<std::string, Gate> gates, int& cutsize, int& AreaProduct);
+#endif // DATASTRUCTURES_HPP
